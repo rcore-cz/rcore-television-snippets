6d8053609839e9b85ad9a0bbd450cb479b5339df
MrXogos <49158267+Xogy@users.noreply.github.com>
2021-06-17 13:20:40 +0200

    fix tabs

--- locales/english/en.lua
+++ locales/english/en.lua
@@ -1,6 +1,5 @@
 Locales = {
     ["help"] = "help\n",
-
     ["doesnt_have_remote"] = "You dont have a remote control to use TV!",
 
     ["tv_help_line_2"] = "Use keys ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to switch default TV program\n",
@@ -9,15 +8,17 @@ Locales = {
     ["tv_help_line_5"] = "To play custom video use command ~g~/playlink~w~\n\n",
     ["tv_help_line_6"] = "To change TV volume use command ~g~/tvvolume~w~\n",
 
-	["volume_changed"] = "You updated prefer TV volume to: %s",
-	["tvstation_changed"] = "You changed TV station to: %s",
+    ["volume_changed"] = "You updated prefer TV volume to: %s",
 
-	["cant_play_video"] = "You need to be in a TV menu to play video!",
-	["link_isnt_whitelisted"] = "You cant use this link ! It is not whitelisted!",
+    ["tvstation_changed"] = "You changed TV station to: %s",
+
+    ["cant_play_video"] = "You need to be in a TV menu to play video!",
+
+    ["link_isnt_whitelisted"] = "You cant use this link ! It is not whitelisted!",
     ["blacklisted_link"] = "This link is blacklisted! Use other one please.",
 
-	["changed_program"] = "You changed TV program",
-	["stopped_program"] = "You stopped the current TV program",
+    ["changed_program"] = "You changed TV program",
+    ["stopped_program"] = "You stopped the current TV program",
 
     ["volume_invalid"] = "The volume has to be between 0-100",
     ["argument_has_to_be_number"] = "The volume has to be a number!",
