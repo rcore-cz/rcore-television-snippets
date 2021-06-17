cd0f9e4ce447964ec42951dc06e2ccc15304bff9
MrXogos <49158267+Xogy@users.noreply.github.com>
2021-06-17 13:19:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# rcore-television-snippets
+---
+
+#### Here are all snippets made by community/rcore dev team!
+
+---
+
+locales
+- english
+
+---
+
+Snippets 
+- nothing yet, want to be first? Make a pull request!
+
+---
+
+API
+- nothing yet, want to be first? Make a pull request!
---
+++ locales/english/en.lua
@@ -0,0 +1,30 @@
+Locales = {
+    ["help"] = "help\n",
+
+    ["doesnt_have_remote"] = "You dont have a remote control to use TV!",
+
+    ["tv_help_line_2"] = "Use keys ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to switch default TV program\n",
+    ["tv_help_line_3"] = "To start selected program press ~INPUT_CELLPHONE_SELECT~\n",
+    ["tv_help_line_4"] = "To end current program press ~INPUT_DIVE~\n\n",
+    ["tv_help_line_5"] = "To play custom video use command ~g~/playlink~w~\n\n",
+    ["tv_help_line_6"] = "To change TV volume use command ~g~/tvvolume~w~\n",
+
+	["volume_changed"] = "You updated prefer TV volume to: %s",
+	["tvstation_changed"] = "You changed TV station to: %s",
+
+	["cant_play_video"] = "You need to be in a TV menu to play video!",
+	["link_isnt_whitelisted"] = "You cant use this link ! It is not whitelisted!",
+    ["blacklisted_link"] = "This link is blacklisted! Use other one please.",
+
+	["changed_program"] = "You changed TV program",
+	["stopped_program"] = "You stopped the current TV program",
+
+    ["volume_invalid"] = "The volume has to be between 0-100",
+    ["argument_has_to_be_number"] = "The volume has to be a number!",
+
+    ["volume_info"] = "Will set a new volume for TV",
+    ["volume_argument"] = "volume",
+
+    ["playlink_info"] = "Will play a custom URL in the TV.",
+    ["play_url_info"] = "Your URL for website",
+}
